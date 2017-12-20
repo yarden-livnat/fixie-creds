64cfdc6737035efd07c62a91c129c396c828af6e
Anthony Scopatz <scopatz@gmail.com>
2017-12-20 14:51:57 -0800

    initial cut of cache

--- fixie_creds/cache.py
+++ fixie_creds/cache.py
@@ -5,6 +5,7 @@ Importantly, while the credtials may generate a token for a user, that token is
 NEVER stored. Instead, the hash of the token is stored.  Verification compares
 the hash of token provided by the user to the stored hashed token.
 """
+import os
 import json
 import random
 import hashlib
@@ -86,7 +87,30 @@ class Cache:
         fname = self.user_cred_file(user)
         with open(fname, 'w') as f:
             json.dump(data, f, sort_keys=True, separators=(',', ':'))
+        os.chmod(fname, 0o600)
         self._users[user] = User(**data)
+
+    def load_user(self, user):
+        """Loads a user into the cache from the filesystem."""
+        fname = self.user_cred_file(user)
+        with open(fname, 'r') as f:
+            data = json.load(f)
+        u = self._users[data['user']] = User(**data)
+        return u
+
+    def get_user(self, user):
+        """Returns the user, loading it from the file system if needed."""
+        u = self._users.get(user, None)
+        if u is None:
+            u = self.load_user(user)
+        return u
+
+    def remove_user(self, user):
+        """Remove the user from the cache and the file system."""
+        self._users.pop(user)
+        fname = self.user_cred_file(user)
+        if os.path.isfile(fname):
+            os.remove(fname)
 
     def register(self, user, email):
         """Registers a new user and provides their token.
@@ -112,6 +136,84 @@ class Cache:
         hashed_token = self.hash_token(token)
         self.write_user(user, email, hashed_token)
         return token, True
+
+    def verify(self, user, token):
+        """Verifies whether or not the user-token pair match.
+
+        Parameters
+        ----------
+        user : str
+            Name of the user.
+        token : str
+            The token to verify for the user.
+
+        Returns
+        -------
+        verified : bool
+            Whether or not the user-token pair is valid.
+        message : str
+            A message string, if needed.
+        flag : bool
+            Whether or not the verification itself could be completed successfully.
+        """
+        if not self.user_exists(user):
+            return False, 'User {0!r} not registered'.format(user), False
+        u = self.get_user(user)
+        hashed_token = self.hash_token(token)
+        valid = (hashed_token == u.hashed_token)
+        msg = 'User verified' if valid else 'Invalid token for user ' + user
+        return valid, msg, True
+
+    def deregister(self, user, token):
+        """Deregisters a user.
+
+        Parameters
+        ----------
+        user : str
+            Name of the user to deregister.
+        token : str
+            The user's token
+
+        Returns
+        -------
+        message : str
+            Message for status of deregistration
+        flag : bool
+            Whether or not the deregistration was successful.
+        """
+        valid, msg, flag = self.verify(user, token)
+        if not valid or not flag:
+            return msg, False
+        self.remove_user(user)
+        return user + ' deregistered', True
+
+    def reset(self, user, email):
+        """Resets a user's token on the system. The email address here must match
+        the one originally provided.
+
+        Parameters
+        ----------
+        user : str
+            Name of the user to register.
+        email : str
+            Email address for the user.
+
+        Returns
+        -------
+        token or message : str
+            The token if the registration was successful, and an error message
+            if it wasn't.
+        flag : bool
+            Whether or not the registration was successful.
+        """
+        if not self.user_exists(user):
+            return 'User {0!r} not registered'.format(user), False
+        u = self.get_user(user)
+        if email != u.email:
+            return 'User email does not match registered email address', False
+        self.remove_user(user)
+        token, flag = self.register(user, email)
+        return token, flag
 
 
 CACHE = Cache()
