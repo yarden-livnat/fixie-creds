6ca5797d3db6873a59732f2e718fd05c2942dccc
Anthony Scopatz <scopatz@gmail.com>
2017-12-20 15:26:44 -0800

    more tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,9 +1,27 @@
 """Tests for cache."""
+import json
 
 from fixie_creds.cache import CACHE
 
 
 def test_register(credsdir):
-    token, flag = CACHE.register('grammaticus', 'john@notaspy.com')
+    user = 'grammaticus'
+    email = 'john@notaspy.com'
+
+    assert not CACHE.user_exists(user)
+
+    token, flag = CACHE.register(user, email)
     assert flag
     assert token == '46685257bdd640fb06671ad11c80317fa3b1799d'
+    assert CACHE.user_exists(user)
+
+    fname = CACHE.user_cred_file(user)
+    with open(fname, 'r') as f:
+        u_from_file = json.load(f)
+    u_from_cache = CACHE.get_user(user)._asdict()
+    assert u_from_file == u_from_cache
+
+    # make sure that a reregister fails
+    msg, flag = CACHE.register(user, email)
+    assert not flag
+    assert msg == "User '" + user + "' already registered"
