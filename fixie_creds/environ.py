75c6d8628bfc712c181afcd6627ca8176b0f0377
Anthony Scopatz <scopatz@gmail.com>
2018-01-02 18:59:52 -0500

    fixe for ordered dict

--- fixie_creds/environ.py
+++ fixie_creds/environ.py
@@ -8,12 +8,10 @@ from fixie.environ import ENV, ENVVARS, expand_and_make_dir
 
 def fixie_creds_dir():
     """Ensures and returns the $FIXIE_CREDS_DIR"""
-    fcd = os.path.join(ENV.get('XDG_DATA_HOME'), 'fixie', 'creds')
+    fcd = os.path.join(ENV.get('FIXIE_DATA_DIR'), 'creds')
     os.makedirs(fcd, exist_ok=True)
     return fcd
 
 
-ENVVARS.update({
-    'FIXIE_CREDS_DIR': (fixie_creds_dir, always_false, expand_and_make_dir,
-                        ensure_string, 'Path to fixie credentials directory'),
-})
+ENVVARS['FIXIE_CREDS_DIR'] = (fixie_creds_dir, always_false, expand_and_make_dir,
+    ensure_string, 'Path to fixie credentials directory')
